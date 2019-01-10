73c3468250288f3cef38f64dcc4cdfdf63590e0d
Norbert Lam <norbertl@buffalo.edu>
2019-01-10 16:58:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,14 @@
 class Post
   attr_accessor :author, :title
+  @@posts = []
   
   def initialize(data)
     @title = data
+    @@posts << self
   end
-
+  
+  def self.count
+    @@posts.size
+  end
 
 end
