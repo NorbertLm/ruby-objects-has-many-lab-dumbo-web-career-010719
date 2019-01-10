e7f9bbfd22ca3341be7bca960e0573c60de1a980
Norbert Lam <norbertl@buffalo.edu>
2019-01-10 16:56:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,5 +11,11 @@ class Author
     self.posts << new_post
     new_post.author = self
   end
+  
+  def add_post_by_title(post_title)
+    new_post = Post.new(post_title)
+    new_post.author = self
+    self.posts << new_post
+  end
 
 end
